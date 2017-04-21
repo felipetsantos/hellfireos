1a63745b46166e54b9f5f2cbd9ab5c7567250800
Felipe Santos <felipe.feteles@gmail.com>
2017-04-21 20:40:53 -0300

    change ap_queue_next logic, it was adding the task again in the queue

--- sys/kernel/scheduler.c
+++ sys/kernel/scheduler.c
@@ -65,8 +65,6 @@ static void ap_queue_next(){
         krnl_task = hf_queue_remhead(krnl_ap_queue);
         if (!krnl_task)
                 panic(PANIC_NO_TASKS_AP);
-        if (hf_queue_addtail(krnl_ap_queue, krnl_task))
-                panic(PANIC_CANT_PLACE_AP);
 
 }
 
